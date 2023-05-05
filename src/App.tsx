c6ef8afd18aca7adf6b473b4c5bcf15b7c886e19
Diane Russel <dianerussel@rogers.com>
2023-05-05 16:50:39 -0400

    Simple page setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,28 @@
-import { useState } from 'react';
-import logo from './assets/images/logo.svg';
+import code from './assets/images/code.svg';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="text-center selection:bg-green-900">
-      <header className="flex min-h-screen flex-col items-center justify-center bg-[#282c34] text-white">
-        <img
-          src={logo}
-          className="animate-speed h-60 motion-safe:animate-spin"
-          alt="logo"
-        />
-        <style>
-          {
-            '\
-            .animate-speed{\
-              animation-duration:20s;\
-            }\
-          '
-          }
-        </style>
-        <p className="bg-gradient-to-r from-emerald-300 to-sky-300 bg-clip-text text-5xl font-black text-transparent selection:bg-transparent">
-          Vite + React + Typescript + Tailwindcss
+    <div className="bg-[#FFDEF6]">
+      <header className="flex min-h-screen flex-col items-center justify-center text-[#E60000]">
+        <img src={code} className="mb-6 w-20" alt="code tag" />
+        <h1 className="mb-8 font-heading text-6xl">
+          Hi, I&apos;m Diane Russel
+        </h1>
+        <p className="text-md">
+          and I&apos;m full stack developer with a passion for making complex
+          things simple.
         </p>
-        <p className="mt-3">
-          <button
-            type="button"
-            className="my-6 rounded bg-gray-300 px-2 py-2 text-[#282C34] transition-all hover:bg-gray-200"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code className="text-[#8d96a7]">App.tsx</code> and save to test
-          HMR updates.
-        </p>
-        <p className="mt-3 flex gap-3 text-center text-[#8d96a7]">
-          <a
-            className="text-[#61dafb] transition-all hover:text-blue-400"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="text-[#61dafb] transition-all hover:text-blue-400"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+
+        <button
+          type="button"
+          className="my-6 rounded border-2 border-[#E60000] bg-white px-2 py-2 transition-all hover:bg-white/70"
+          onClick={(e) => {
+            window.location.href = 'mailto:dianerussel@rogers.com';
+            e.preventDefault();
+          }}
+        >
+          Get in touch
+        </button>
       </header>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
+    fontFamily: {
+      'heading': ['ui-serif', 'Georgia'],
+    },
     extend: {},
   },
   plugins: [],
