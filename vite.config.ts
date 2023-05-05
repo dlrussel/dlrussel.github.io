57731d3597dba373680a14ffa38af761439d02e2
Diane Russel <dianerussel@rogers.com>
2023-05-05 13:04:45 -0400

    vite project setup

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import eslintPlugin from '@nabla/vite-plugin-eslint';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+
+export default defineConfig({
+  base:'/',
+  plugins: [react(), eslintPlugin()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
