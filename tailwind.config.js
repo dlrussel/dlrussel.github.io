93281d7b4ce5357f4bf7724cba6879ca397d9d35
Diane Russel <dianerussel@rogers.com>
2023-05-06 14:39:16 -0400

    animation cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,17 +12,22 @@ module.exports = {
           '0%, 100%': { transform: 'translateY(0)' },
           '50%': { transform: 'translateY(1rem)' },
         },
+        'float-less': {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(0.7rem)' },
+        },
         'shift-left': {
           '0%, 100%': { transform: 'translateX(0)' },
-          '50%': { transform: 'translateX(-2rem)' },
+          '50%': { transform: 'translateX(-1.5rem)' },
         },
         'shift-right': {
           '0%, 100%': { transform: 'translateX(0)' },
-          '50%': { transform: 'translateX(1rem)' },
+          '50%': { transform: 'translateX(1.6rem)' },
         },
       },
       animation: {
         float: 'float 3s ease-in-out infinite',
+        'float-less': 'float-less 3s ease-in-out infinite',
         'shift-left': 'shift-left 4s ease-in-out infinite',
         'shift-right': 'shift-right 4s ease-in-out infinite',
       },
