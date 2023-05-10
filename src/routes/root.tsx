3d47fb8014a3439b3696bcebcbccf856a70aed07
Diane Russel <dianerussel@rogers.com>
2023-05-10 18:55:50 -0400

    add navigation

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,4 +1,5 @@
 import { Outlet, NavLink } from 'react-router-dom';
+import classNames from 'classnames';
 
 import { ReactComponent as Code } from '../assets/images/code.svg';
 import { ReactComponent as GithubLogo } from '../assets/images/github-logo.svg';
@@ -8,30 +9,31 @@ export default function Root() {
   return (
     <>
       <header className="flex items-center justify-between bg-[#FFDEF6] px-8 py-8 text-red">
-        <nav className="flex items-center">
-          <Code className="w-16 pr-5 md:w-24" />
+        <nav className="flex items-center font-bold">
+          <Code className="mr-7 w-16 md:w-20" />
+
           <NavLink
             to="/"
-            className="pr-5 font-bold"
-            style={({ isActive }) => {
-              return {
-                textDecoration: isActive ? 'underline' : 'none',
-                color: isActive ? '#ae0000' : '#e60000',
-              };
-            }}
+            className={({ isActive }) =>
+              classNames('mr-5', 'border-b-4', 'hover:text-[#ae0000]', {
+                'border-transparent': !isActive,
+                'border-[#ae0000]': isActive,
+                'text-[#ae0000]': isActive,
+              })
+            }
             end
           >
             Home
           </NavLink>
           <NavLink
-            className="font-bold"
             to="/about"
-            style={({ isActive }) => {
-              return {
-                textDecoration: isActive ? 'underline' : 'none',
-                color: isActive ? '#ae0000' : '#e60000',
-              };
-            }}
+            className={({ isActive }) =>
+              classNames('border-b-4', 'hover:text-[#ae0000]', {
+                'border-transparent': !isActive,
+                'border-[#ae0000]': isActive,
+                'text-[#ae0000]': isActive,
+              })
+            }
           >
             About
           </NavLink>
