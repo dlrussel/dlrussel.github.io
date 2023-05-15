bf0c4d7b5b8a69028a801f88033ec2d1e8434163
Diane Russel <dianerussel@rogers.com>
2023-05-15 16:08:01 -0400

    update nav design

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -8,53 +8,70 @@ import { ReactComponent as LinkedinLogo } from '../assets/images/linkedin-logo.s
 export default function Root() {
   return (
     <>
-      <header className="flex items-center justify-between bg-pink px-8 py-8 text-red">
-        <nav className="flex items-center font-fira-code font-bold">
-          <Code className="mr-7 w-16 md:w-20" />
-
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              classNames('mr-5', 'border-b-4', 'hover:text-[#ae0000]', {
-                'border-transparent': !isActive,
-                'border-[#ae0000]': isActive,
-                'text-[#ae0000]': isActive,
-              })
-            }
-            end
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/about"
-            className={({ isActive }) =>
-              classNames('border-b-4', 'hover:text-[#ae0000]', {
-                'border-transparent': !isActive,
-                'border-[#ae0000]': isActive,
-                'text-[#ae0000]': isActive,
-              })
-            }
-          >
-            About
-          </NavLink>
+      <header className="flex items-center justify-between bg-pink px-8 py-8 text-off-black">
+        <nav className="flex w-full items-center justify-between rounded-3xl border-[1px] border-off-black bg-white px-7 py-3 font-fira-code font-bold">
+          <div className="flex items-center">
+            <Code className="w-8 fill-off-black md:w-8" />
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                classNames(
+                  'ml-5',
+                  'border-[1px]',
+                  'border-off-black',
+                  'px-6',
+                  'py-2',
+                  'hover:drop-shadow-[3px_3px_rgba(52,43,50,1)]',
+                  'rounded-2xl',
+                  'bg-white',
+                  {
+                    'drop-shadow-[3px_3px_rgba(52,43,50,1)]': isActive,
+                  },
+                )
+              }
+              end
+            >
+              Diane Russel
+            </NavLink>
+          </div>
+          <div className="flex items-center">
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                classNames(
+                  'mr-5',
+                  'border-[1px]',
+                  'border-off-black',
+                  'px-6',
+                  'py-2',
+                  'hover:drop-shadow-[3px_3px_rgba(52,43,50,1)]',
+                  'rounded-2xl',
+                  'bg-white',
+                  {
+                    'drop-shadow-[3px_3px_rgba(52,43,50,1)]': isActive,
+                  },
+                )
+              }
+            >
+              About
+            </NavLink>
+            <a
+              href="https://github.com/dlrussel"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mr-3"
+            >
+              <GithubLogo className="h-8 w-8 fill-off-black hover:fill-[#ae0000]" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/diane-russel/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <LinkedinLogo className="h-8 w-8 fill-off-black hover:fill-[#ae0000]" />
+            </a>
+          </div>
         </nav>
-        <div className="flex">
-          <a
-            href="https://github.com/dlrussel"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mr-3"
-          >
-            <GithubLogo className="h-10 w-10 fill-red hover:fill-[#ae0000]" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/diane-russel/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <LinkedinLogo className="h-10 w-10 fill-red hover:fill-[#ae0000]" />
-          </a>
-        </div>
       </header>
       <main>
         <Outlet />
