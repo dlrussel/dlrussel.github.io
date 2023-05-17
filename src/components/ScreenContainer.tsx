3931d1769b9b041c31eeeb449194f172d9e70e73
Diane Russel <dianerussel@rogers.com>
2023-05-17 14:08:24 -0400

    use container component for about me and work experience sections

--- src/components/ScreenContainer.tsx
+++ src/components/ScreenContainer.tsx
@@ -3,10 +3,10 @@ import { ReactNode } from 'react';
 export default function ScreenContainer({
   children,
 }: {
-  children?: [ReactNode];
+  children?: [ReactNode] | ReactNode;
 }) {
   return (
-    <div className="flex flex-col justify-stretch rounded-xl border-4 border-[#f1f1f1] bg-white font-fira-code drop-shadow-2xl">
+    <div className="flex flex-col justify-stretch rounded-xl border-4 border-[#f1f1f1] bg-white font-fira-code drop-shadow-xl">
       <div className="bg-[#f1f1f1] p-4">
         <span className="mr-2 inline-block h-5 w-5 rounded-full bg-[#bbbbbb]"></span>
         <span className="mr-2 inline-block h-5 w-5 rounded-full bg-[#bbbbbb]"></span>
@@ -15,7 +15,6 @@ export default function ScreenContainer({
       <div className="flex h-full flex-col items-center justify-center bg-white p-10 font-fira-code">
         {children}
       </div>
-      ;
     </div>
   );
 }
